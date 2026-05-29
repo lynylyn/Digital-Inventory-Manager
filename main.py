fdb32a21bf7b69bc1050c4c25be426e59e8573f6
lynylyn <evelyn.starling@education.nsw.gov.au>
2026-05-29 14:44:49 +1000

    Debugging
    Drastically changed the code, wasn't really understanding it before but it does make sense now

--- main.py
+++ main.py
@@ -6,49 +6,59 @@ class Product:
         self.quantity = quantity
 
     def update_stock(self, amount):
-        if self.quantity + amount < 0:
-            print("The quantity given was invalid.")
+        if self.quantity + amount >= 0:
+            self.quantity += amount
         else:
-            self.quantity.append(self.quantity + amount)
-            return self.quantity
+            print("The quantity given was invalid.")
 
     def get_total_value(self):
-        total_value = self.price * self.quantity
-        return total_value
+        return self.price * self.quantity
 
     def __str__(self):
-        return f"The total value for your order of {self.quantity} {self.name}/s was {self.get_total_value}."
+        return f"ID: {self.product_id} | {self.name} | ${self.price} | Stock: {self.quantity}"
 
 class Inventory:
     def __init__(self, products):
-        self.products = products
-        products = {Product.product_id: Product.name}
+        self.products = {}
 
-    def add_product(self, product_id, name):
-        self.products.update({product_id: name})
+    def add_product(self, product):
+        self.products[product_id].update_stock(amount)
 
     def display_all(self):
-        for product in self.products:
-            Product.__str__(product)
+        for product in self.products.values():
+            print(product)
 
-#if __name__ == "__main__":
-    #print("Welcome to the Inventory Manager.")
-    #input = input("\n(1) Add New Product\n(2) View All Inventory\n(3) Update Product Stock (Buy/Sell)\n(4) Exit\n")
-    #if input == "1":
-        #name = input("Please enter the name of the product you'd like to add.")
-        #product_id = input("Please enter the id of the product you'd like to add.")
-        #Inventory.add_product(Inventory, product_id, name)
-        #print("\nAdded successfully.")
-    #if input == "2":
-        #Inventory.display_all(Inventory)
-        #print("\nCompleted.")
-    #if input == "3":
-        #amount = input("Please input the amount you'd like to update the stock by.")
-        #Product.update_stock(Product, amount)
-    #if input == "4":
-        #exit
-    #else:
-        #print("The selection was invalid.")
+if __name__ == "__main__":
+    print("Welcome to the Inventory Manager.")
+    print("\n(1) Add New Product\n(2) View All Inventory\n(3) Update Product Stock (Buy/Sell)\n(4) Exit\n")
+
+    choice = input("Choose: ")
+
+    if choice == "1":
+        name = input("\nPlease enter the name of the product you'd like to add: ")
+        product_id = input("\nPlease enter the id of the product: ")
+        price = float(input("\nPlease enter the price of the product: "))
+        quantity = int(input("\nPlease enter the quantity of the product: "))
+
+        product = Product(product_id, name, price, quantity)
+        Inventory.add_product(product)
+        print("\nAdded successfully.")
+
+    if choice == "2":
+        Inventory.display_all()
+        print("\nCompleted.")
+
+    if input == "3":
+        product_id = input("\nEnter product ID: ")
+        amount = int(input("\n Enter the amount you'd like to update the stock by: "))
+        Product.update_stock(product_id, amount)
+
+    if input == "4":
+        print("Exiting...")
+        break
+
+    else:
+        print("The selection was invalid.")
 
 object = Product(12, "car", 500, 2)
 print(Product.__str__(object))
